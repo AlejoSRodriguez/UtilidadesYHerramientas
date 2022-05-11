18a439b2b2ae9b5af7c8b433e411707730908a23
JavierMartorano <martorano.javi@gmail.com>
2022-05-11 14:04:27 -0300

    mi primer commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         asdasdasd PUTOOOOO
+         NACHITO PUTOOOOO
         </p>
         <a
           className="App-link"
