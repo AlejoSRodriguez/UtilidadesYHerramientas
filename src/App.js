6ad0fa0300820fbc4b9c50acc098c8406e060fd9
JavierMartorano <martorano.javi@gmail.com>
2022-05-11 13:59:26 -0300

    mi priemr commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         NACHITO PUTOOOOO
+         asdasdasd PUTOOOOO
         </p>
         <a
           className="App-link"
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> 
     </div>
   );
 }
