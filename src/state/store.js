08716615111dd4291213457a15c42c421f17df11
JavierMartorano <martorano.javi@gmail.com>
2022-05-12 13:49:10 -0300

    folders

---
+++ src/state/store.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+
+
+const store = configureStore({
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  reducer: {
+  },
+});
+
+export default store;
